c61105ecccdc67d6cefcf7adf44517614b42a448
xsamrata <lexuan2007nt@gmail.com>
2026-07-19 23:42:30 +0700

    Update admin.cpp

--- admin.cpp
+++ admin.cpp
@@ -1,1 +1,15 @@
+#include "admin.h"
+#include <QDebug>
 
+Admin::Admin() {}
+
+bool Admin::createNewAccount(QString username, QString password)
+{
+    if (!User::GetActiveUser().hasPermission(Permission::manageUsers))
+    {
+        qDebug() << "Access Denied: Current user missing 'manageUsers' permission.";
+        return false;
+    }
+    qDebug() << "Account created successfully: " << username;
+    return true;
+}
