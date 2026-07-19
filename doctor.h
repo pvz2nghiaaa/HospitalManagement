5205a5e76a54b8a1a743d5dfe933f4304703372e
xsamrata <lexuan2007nt@gmail.com>
2026-07-20 00:50:22 +0700

    Update doctor.h

--- doctor.h
+++ doctor.h
@@ -2,10 +2,9 @@
 #include "user.h"
 #include "permission.h"
 
-class Doctor
+class Doctor : public User
 {
 public:
-    Doctor();
-    static bool createMedicalRecord(int patientId, QString diagnosis);
-    static bool prescribeMedicine(int patientId, QString medicineName);
+    Doctor(int nID, QString username, QString password, QString fullName, QString phone, bool isActive);
+    bool prescribeMedicine(int patientId, QString medicineName);
 };
