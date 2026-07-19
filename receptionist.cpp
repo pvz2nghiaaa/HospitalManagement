9b33fac264497aaea5f95976b83b5c61d5af2f0d
xsamrata <lexuan2007nt@gmail.com>
2026-07-19 23:43:57 +0700

    Update receptionist.cpp

--- receptionist.cpp
+++ receptionist.cpp
@@ -1,1 +1,16 @@
+#include "receptionist.h"
+#include <QDebug>
 
+Receptionist::Receptionist() {}
+
+bool Receptionist::registerNewPatient(QString patientName)
+{
+    if (!User::GetActiveUser().hasPermission(Permission::createPatient))
+    {
+        qDebug() << "Access Denied: Current user missing 'createPatient' permission.";
+        return false;
+    }
+
+    qDebug() << "New patient registered:" << patientName;
+    return true;
+}
