8b629c89f7f91d0ac25f21066deb24fd844126ca
smileee <smile211107@gmail.com>
2026-07-25 00:32:57 +0700

    Add: doctor header with method declarations

--- doctor.h
+++ doctor.h
@@ -2,9 +2,31 @@
 #include "user.h"
 #include "permission.h"
 
-class Doctor : public User
-{
+class Doctor : public User{
 public:
     Doctor(int nID, QString username, QString password, QString fullName, QString phone, bool isActive);
     bool prescribeMedicine(int patientId, QString medicineName);
+
+    static QList<Patient> SearchPatientBy(const QString& keyword);
+    static Patient GetPatientDetails(int patientID);
+    static bool UpdatePatientInfo(int id, const QString& fullName, const QString& birthDate, const QString& sex, const QString& address);
+
+    static QList<QString> GetDiseasesList();
+    static bool AddNewDisease(const QString& conditionName, const QString& icdCode);
+    static bool SaveDiagnosis(int recordId, const QString& conditionName, const QString& icdCode, const QString& severity, const QString& clinicalNote, const QString& dateDiagnosed);
+    static bool MarkRecordComplete(int recordId);
+
+    static QList<QString> GetDrugsList();
+    static bool AddPrescriptionItem(int recordId, int drugId, int quantity, const QString& instruction);
+    static bool RemovePrescriptionItem(int prescriptionItemId);
+    static bool SavePrescription(int recordId, const QString& dateIssued, const QList<PrescriptionItem>& items);
+
+    static QList<MedicalRecord> SearchRecordsBy(const QString& keyword, const QString& status, const QString& date);
+    static MedicalRecord GetRecordDetails(int recordId);
+    static QList<PrescriptionItem> GetRecordPrescriptions(int recordId);
+    static void PrintRecord(int recordId);
+
+    static Profile GetMyProfileInfo();
+    static QList<Permission> GetMyPermissions();
+    static QList<ActivityLog> SearchMyActivityLogs(const QString& dateFrom, const QString& dateTo);
 };
