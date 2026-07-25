105f4ff00ffae9a7b16a9a2ba2e319137ae97ecd
shin-hybrid <phukun1991@gmail.com>
2026-07-25 22:07:35 +0700

    add stock quantity

--- drug.h
+++ drug.h
@@ -11,12 +11,16 @@ private:
     QString name;
     QString unit;
     double price;
+    int stockQuantity;
 
 public:
     // Constructors
     Drug();
     Drug(QString name, QString unit, double price);
+    Drug(QString name, QString unit, double price, int stockQuantity);
     Drug(int drugID, QString name, QString unit, double price);
+    Drug(int drugID, QString name, QString unit,double price, int stockQuantity);
+
 
     // Database initialization
     static bool initTable();
@@ -26,16 +30,19 @@ public:
     QString getName() const;
     QString getUnit() const;
     double getPrice() const;
+    int getStockQuantity() const;
 
     // Setters
     void setDrugID(int id);
     void setName(const QString &nName);
     void setUnit(const QString &nUnit);
     void setPrice(double nPrice);
+    void setStockQuantity(int quantity);
 
     // Fluent Setters
     Drug& SetDrugID(int id);
     Drug& SetName(const QString &nName);
     Drug& SetUnit(const QString &nUnit);
     Drug& SetPrice(double nPrice);
+    Drug& SetStockQuantity(int quantity);
 };
