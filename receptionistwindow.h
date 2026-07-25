8a6171cd45fb330e62b07d9c3ed5c94a11274588
shin-hybrid <phukun1991@gmail.com>
2026-07-25 22:26:03 +0700

    drug management code

--- receptionistwindow.h
+++ receptionistwindow.h
@@ -29,9 +29,31 @@ private slots:
 
     void on_btnLogout_clicked();
 
+ // Drug Management
+    void on_btnSearch_14_clicked();
+    void on_btnSearch_15_clicked();
+    void on_btnSearch_16_clicked();
+    void on_btnSearch_17_clicked();
+    void on_btnSearch_18_clicked();
+    void on_tblPatient_2_cellClicked(int row, int column);
+
 private:
     Ui::ReceptionistWindow *ui;
     void navigateToPage(int pageIndex, QPushButton* activeBtn);
+
+ // Drug Functions
+    void SearchDrugsBy(const QString &nameOrID,const QString &stockStatus);
+
+    bool UpdateDrugInfo(int drugID,const QString &name,const QString &unit,double price,int stockQuantity);
+
+    bool AddNewDrug(const QString &name,const QString &unit,double price,int stockQuantity);
+
+    void loadAllDrugs();
+
+    QString getDrugStatus(int stockQuantity) const;
+
+    void loadSelectedDrugToEdit(int row);
+
 };
 
 #endif // RECEPTIONISTWINDOW_H
