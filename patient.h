817b9ea8da7fa3dec76d761b31e33138aa6f46cc
xsamrata <lexuan2007nt@gmail.com>
2026-07-26 23:54:39 +0700

    Update patient.h
    add some variables to Patient class

--- patient.h
+++ patient.h
@@ -1,14 +1,24 @@
+#pragma once
 #include <QSqlQuery>
 #include <QDebug>
+#include <QString>
+#include <QVariant>
 
 class Patient {
 public:
+    int ID = -1;
+    QString FullName;
+    QString BirthDate;
+    QString Sex;
+    QString Address;
+
     static bool initTable() {
         QSqlQuery q;
         return q.exec("CREATE TABLE IF NOT EXISTS Patients ("
                       "ID INTEGER PRIMARY KEY AUTOINCREMENT, "
                       "FullName TEXT, BirthDate TEXT, Sex TEXT, Address TEXT)");
     }
+
     static int GetTotalPatients(){
         QSqlQuery query;
         query.prepare("SELECT COUNT(*) AS totalPatients FROM Patients");
