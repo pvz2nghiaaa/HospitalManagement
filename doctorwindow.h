76e290bf9a111991de972d06ed25a186697b3ac3
smileee <smile211107@gmail.com>
2026-07-28 13:09:47 +0700

    merge

--- doctorwindow.h
+++ doctorwindow.h
@@ -26,17 +26,15 @@ private slots:
     void on_btnDoctors_clicked();
     void on_btnLogout_clicked();
 
-<<<<<<< HEAD
     void on_btnSearch_5_clicked();
     void on_btnRefreshRecords_clicked();
     void on_tblMedicalRecords_cellClicked(int row, int column);
     void on_btnPrintRecord_clicked();
     void on_btnCloseRecord_clicked();
-=======
+    
     void on_btnSearch_4_clicked();
     void on_tblPatient_cellClicked(int row, int column);
     void on_btnUpdatePatient_clicked();
->>>>>>> f196fc5d079b65f1fcd71665049ee813be7d7697
 
 private:
     Ui::DoctorWindow *ui;
